774659b56ebfdda68f2635d8fc147f78ec22cad1
Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>
2024-05-16 13:23:56 +0200

    launch_tests: adapt script to playbook refactoring
    As playbooks names have changed, adapt the launch test script with
    these modifications.
    
    Signed-off-by: Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>

--- launch_tests.sh
+++ launch_tests.sh
@@ -25,19 +25,18 @@ it="$1"
 pcap_cyles="$2"
 dev="$3"
 
-sed -i "s/^\(\s\{28\}pcap_cycles:\s*\)[0-9]\+/\1$pcap_cyles/" seapath-inventories/fromveur_2024.yaml
+sed -i "s/^\(\s\{28\}pcap_cycles:\s*\)[0-9]\+/\1$pcap_cyles/" seapath-inventories/fromveur/common.yaml
 
 for ((i=1; i<=it; i++)); do
     ssh $SSH_OPTS $VM_HOST reboot
     sleep 120
 
     if [ "$dev" == "vm" ]; then
-        ansible-playbook -i seapath-inventories/fromveur_2024.yaml playbooks/vms_configure_latency_tests.yaml
-        ansible-playbook -i seapath-inventories/fromveur_2024.yaml playbooks/vms_run_latency_tests.yaml
+        ansible-playbook -i seapath-inventories/fromveur/common.yaml -i seapath-inventories/fromveur/vms.yaml playbooks/configure_latency_tests.yaml
+        ansible-playbook -i seapath-inventories/fromveur/common.yaml -i seapath-inventories/fromveur/vms.yaml playbooks/run_latency_tests.yaml
     else
-        ansible-playbook -i seapath-inventories/fromveur_2024.yaml playbooks/hyp_configure_latency_tests.yaml
-        ansible-playbook -i seapath-inventories/fromveur_2024.yaml playbooks/hyp_run_latency_tests.yaml
-
+        ansible-playbook -i seapath-inventories/fromveur/common.yaml -i seapath-inventories/fromveur/hyp.yaml playbooks/configure_latency_tests.yaml
+        ansible-playbook -i seapath-inventories/fromveur/common.yaml -i seapath-inventories/fromveur/hyp.yaml playbooks/run_latency_tests.yaml
     fi
     mv tests_results tests_results-$(date +%F_%Hh%Mm%S)
 done
